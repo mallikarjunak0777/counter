8b4eee5953835a398d5d2a6abaca9d60008f8c2c
mallikarjunak0777 <32353304+mallikarjunak0777@users.noreply.github.com>
2023-07-11 23:46:59 +0530

    Update ag-grid-2.js

--- ag-grid-2.js
+++ ag-grid-2.js
@@ -19,6 +19,35 @@ const Table = () => {
       prevData.map((row, i) => (i === index ? { ...row, [name]: value } : row))
     );
   };
+  function validateThreshold(data) {
+  const filteredData = data.filter((obj, index) => {
+    if (index === data.length - 1) {
+      return false; // Exclude the last object
+    }
+
+    if (obj.highthreshValue === '') {
+      return false; // Exclude objects with empty highthreshValue
+    }
+
+    const currhighthreshValue = parseFloat(obj.highthreshValue);
+    const currlowthreshlod = parseFloat(obj.lowthreshValue);
+
+    return currhighthreshValue !== undefined && currhighthreshValue >= currlowthreshlod;
+  });
+
+  return filteredData;
+}
+
+// Example usage
+const arr = [
+  { id: 1, highthreshValue: '10', lowthreshValue: '5' },
+  { id: 2, highthreshValue: '8', lowthreshValue: '6' },
+  { id: 3, highthreshValue: '', lowthreshValue: '7' }
+];
+
+const filteredArr = validateThreshold(arr);
+console.log(filteredArr);
+
 
   return (
     <div>
