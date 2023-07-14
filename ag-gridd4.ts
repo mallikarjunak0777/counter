2cb5e66c94d6af6e51506b68182b1505bcdfc7ec
mallikarjunak0777 <32353304+mallikarjunak0777@users.noreply.github.com>
2023-07-14 14:54:34 +0530

    Update ag-gridd4.ts

--- ag-gridd4.ts
+++ ag-gridd4.ts
@@ -1,104 +1,145 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Table = () => {
-  const [data, setData] = useState([
-    { id: 1, name: '', age: '' },
-    { id: 2, name: '', age: '' },
-    { id: 3, name: '', age: '' },
-    { id: 4, name: '', age: '' },
-  ]);
+const initialData = [
+  { rate: '10', low: '1', high: '2' },
+  { rate: '20', low: '3', high: '4' },
+  { rate: '30', low: '5', high: '6' }
+];
 
-  const handleInputChange = (e, index, columnName) => {
-    const { value } = e.target;
-    setData((prevData) =>
-      prevData.map((row, i) => {
-        if (i === index) {
-          return { ...row, [columnName]: value };
-        }
-        return row;
-      })
+const App = () => {
+  const [data, setData] = useState(initialData);
+  const [initialErrors, setInitialErrors] = useState(
+    initialData.map(() => ({
+      rate: '',
+      high: ''
+    }))
+  );
+  const [errors, setErrors] = useState(initialData.map(() => ({
+    rate: '',
+    high: ''
+  }))
+);
+  const [isSaveDisabled, setSaveDisabled] = useState(true);
+
+  useEffect(() => {
+    checkSaveButtonDisabled();
+  }, [data]);
+
+  const handleRateChange = (index, value) => {
+    const newData = [...data];
+    newData[index] = { ...newData[index], rate: value };
+    setData(newData);
+    validateCell(index, 'rate', value,newData);
+  };
+
+  const handleHighChange = (index, value) => {
+    const newData = [...data];
+    newData[index] = { ...newData[index], high: value };
+    if (index < newData.length - 1) {
+      newData[index + 1] = { ...newData[index + 1], low: (parseInt(value, 10) + 1).toString() };
+    }
+    setData(newData);
+    validateCell(index, 'high', newData[index].low);
+  };
+
+  const handleClearRate = () => {
+    const newData = data.map(row => ({ ...row, rate: '' }));
+    setData(newData);
+  };
+
+  const handleClearLowHigh = () => {
+    const newData = data.map(row => ({ ...row, low: '', high: '' }));
+    setData(newData);
+  };
+
+  const handleClearAll = () => {
+    setData(initialData);
+    setErrors(initialErrors);
+  };
+
+  const handleSave = () => {
+    // Handle save functionality here
+    console.log('Data saved:', data);
+  };
+
+  const checkSaveButtonDisabled = () => {
+    const isAnyCellEmpty = data.some(row => row.rate === '' || row.low === '' || row.high === '');
+    const isAnyCellInvalid = data.some(
+      row => row.rate > 100 || (row.high !== '' && parseInt(row.high, 10) <= parseInt(row.low, 10))
     );
+    const isDataUpdated = JSON.stringify(data) !== JSON.stringify(initialData);
+    setSaveDisabled(isAnyCellEmpty || isAnyCellInvalid || !isDataUpdated);
   };
 
-  const validateName = (name) => {
-    // Example validation: Name must be at least 3 characters long
-    return name.trim().length === 0 || name.length >= 3;
-  };
+  const validateCell = (index, field, compareValue,newdata) => {
+    const newErrors = [...errors];
 
-  const validateAge = (age, currentIndex) => {
-    // Example validation: Age must be greater than the previous row's age
-    if (age === '') {
-      return true; // Allow empty value
+    if (newdata[index][field] === '') {
+      newErrors[index][field] = `${field.toUpperCase()} is missing`;
+    } else if (field === 'rate' && (newdata[index][field] < 0 || newdata[index][field] > 100)) {
+      newErrors[index][field] = `${field.toUpperCase()} should be between 0 and 100`;
+    } else if (field === 'rate' && newdata[index][field] === initialData[index][field]) {
+      newErrors[index][field] = 'Current data is the same as initial data';
+    } else if (field === 'high' && (newdata[index][field] !== '' && newdata[index][field] <= compareValue)) {
+      newErrors[index][field] = `${field.toUpperCase()} should be greater than Low`;
+    } else {
+      newErrors[index][field] = '';
     }
-    const previousAge = data[currentIndex - 1]?.age;
-    return previousAge === undefined || (previousAge !== '' && parseInt(age) > parseInt(previousAge));
+    console.log("==>initialData",initialData[index][field] );
+  console.log("==>data",newdata[index].rate);
+    setErrors(newErrors);
   };
 
-  const hasError = (value, columnName, currentIndex) => {
-    switch (columnName) {
-      case 'name':
-        return !validateName(value);
-      case 'age':
-        return !validateAge(value, currentIndex);
-      default:
-        return false;
-    }
-  };
+  
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Age</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <TableRow
-            key={item.id}
-            data={item}
-            index={index}
-            handleInputChange={handleInputChange}
-            hasError={hasError}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Rate</th>
+            <th>Low</th>
+            <th>High</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td>
+                <input
+                  type="number"
+                  value={row.rate}
+                  onChange={e => handleRateChange(index, e.target.value)}
+                />
+                <div className="error">{errors[index].rate}</div>
+              </td>
+              <td>{row.low}</td>
+              <td>
+                <input
+                  type="number"
+                  value={row.high}
+                  onChange={e => handleHighChange(index, e.target.value)}
+                />
+                <div className="error">{errors[index].high}</div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={handleClearRate}>Clear Rate</button>
+      <button
+        onClick={handleClearLowHigh}
+        disabled={data.some(row => row.low === '' || row.high === '')}
+      >
+        Clear Low/High
+      </button>
+      <button onClick={handleClearAll}>Clear All</button>
+      <button onClick={handleSave} disabled={isSaveDisabled}>
+        Save
+      </button>
+    </div>
   );
 };
 
-const TableRow = ({ data, index, handleInputChange, hasError }) => {
-  const { id, name, age } = data;
+export default App;
 
-  return (
-    <tr>
-      <td>{id}</td>
-      <td>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => handleInputChange(e, index, 'name')}
-          className={hasError(name, 'name', index) ? 'error' : ''}
-        />
-        {hasError(name, 'name', index) && name.trim().length > 0 && (
-          <span className="error-message">Invalid Name</span>
-        )}
-      </td>
-      <td>
-        <input
-          type="text"
-          value={age}
-          onChange={(e) => handleInputChange(e, index, 'age')}
-          className={hasError(age, 'age', index) ? 'error' : ''}
-        />
-        {hasError(age, 'age', index) && (
-          <span className="error-message">Invalid Age</span>
-        )}
-      </td>
-    </tr>
-  );
-};
-
-export default Table;
